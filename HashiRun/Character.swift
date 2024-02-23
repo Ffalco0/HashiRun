c85e045c188144b9ae5f1a5c203a8b47bd47656d
Ffalco0 <142512909+Ffalco0@users.noreply.github.com>
2024-02-23 16:35:18 +0100

    add new file and character

---
+++ HashiRun/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  HashiRun
+//
+//  Created by Fabio Falco on 23/02/24.
+//
+
+import SwiftUI
+
+struct Character: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Character()
+}
