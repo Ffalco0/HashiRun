985f85e64e9f61c18de2c1ec7172b0392a61a545
Ffalco0 <142512909+Ffalco0@users.noreply.github.com>
2024-02-23 14:21:52 +0100

    Initial Commit

---
+++ HashiRun/HashiRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashiRunApp.swift
+//  HashiRun
+//
+//  Created by Fabio Falco on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HashiRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
