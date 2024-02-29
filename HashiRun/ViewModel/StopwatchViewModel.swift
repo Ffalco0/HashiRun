3fa42d5e843e2ccd8704798eb2617e08cccf1e76
OvrLore <lorenzoovera@gmail.com>
2024-02-29 15:14:02 +0100

    added stopwatchviewmodel

--- HashiRun/ViewModel/StopwatchViewModel.swift
+++ HashiRun/ViewModel/StopwatchViewModel.swift
@@ -1,18 +1,33 @@
 //
-//  StopwatchViewModel.swift
-//  HashiRun
+//  StopWatchViewModel.swift
+//  RunningRPG
 //
-//  Created by Lorenzo Overa on 28/02/24.
+//  Created by Lorenzo Overa on 20/02/24.
 //
-
 import SwiftUI
 
-struct StopwatchViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+class StopwatchViewModel: ObservableObject {
+    @Published var seconds = 0
+    @State var timer: Timer?
 
-#Preview {
-    StopwatchViewModel()
+    func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+            self?.updateTime()
+        }
+    }
+
+    func pause() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+        seconds = 0
+    }
+
+    func updateTime() {
+        seconds += 1
+    }
 }
