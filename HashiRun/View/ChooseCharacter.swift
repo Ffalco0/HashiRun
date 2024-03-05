d1f752a5638a7f0685d76a78ad5c5da2b01920df
Ffalco0 <142512909+Ffalco0@users.noreply.github.com>
2024-03-05 11:28:23 +0100

    make the history work for specific session add the character selection after the firdt run

---
+++ HashiRun/View/ChooseCharacter.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseCharacter.swift
+//  HashiRun
+//
+//  Created by Fabio Falco on 05/03/24.
+//
+
+import SwiftUI
+
+struct ChooseCharacter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseCharacter()
+}
